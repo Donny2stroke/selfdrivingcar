fc6f604e8aac06e3e4b04a87174ab488ffadcdd0
Claudio <claudio2000@hotmail.it>
2024-11-20 16:44:17 +0100

    Controllo collisione bordo strada

--- car.js
+++ car.js
@@ -13,11 +13,51 @@ class Car{
 
         this.angle = 0;
 
+        this.damaged=false;
+
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update(){
-        this.#move();
+    update(roadBorders){
+        if(!this.damaged){
+            this.#move();
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
+        }
+        this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move(){
@@ -63,7 +103,22 @@ class Car{
     }
 
     draw(ctx){
-        ctx.save();
+
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle="black";
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i=1;i<this.polygon.length;i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
+        ctx.fill();
+
+        //VECCHIO DISEGNO
+        /* ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
         ctx.beginPath();
@@ -74,6 +129,9 @@ class Car{
             this.height
         );
         ctx.fill();
-        ctx.restore();
+        ctx.restore(); */
+
+        this.sensor.draw(ctx);
+
     }
 }
